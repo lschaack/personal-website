52f6a148176ebdca16042bbfb3363fc94f9d8230
lschaack <luke.schaack@gmail.com>
2018-03-08 14:35:23 -0700

    lots of work on the look, got the color script working for the menu but the colors are super ugly

--- scripts/general.js
+++ scripts/general.js
@@ -1,6 +1,34 @@
 "use strict";
 var UP_ARROW = '↑';
 var DOWN_ARROW = '↓';
+function doPrettyColors() {
+    var ids = ["first", "second", "third", "fourth", "fifth"];
+    for (var _i = 0, ids_1 = ids; _i < ids_1.length; _i++) {
+        var id = ids_1[_i];
+        var container = document.getElementById(id);
+        var children = container.children;
+        var topColor = window.getComputedStyle(children[0]).backgroundColor;
+        console.log(topColor);
+        for (var childIndex in children) {
+            console.log(children);
+            console.log(childIndex);
+            console.log(children[childIndex]);
+            if (parseInt(childIndex)) {
+                var currElement = children[childIndex];
+                currElement.style.backgroundColor = decrementLightness(topColor, parseInt(childIndex) * 0.1);
+            }
+        }
+    }
+}
+function decrementLightness(colorString, percent) {
+    var r = parseInt(colorString.substring(4, colorString.indexOf(',')));
+    var g = parseInt(colorString.substring(colorString.indexOf(',') + 1, colorString.lastIndexOf(',')));
+    var b = parseInt(colorString.substring(colorString.lastIndexOf(',') + 1));
+    r = r * (1 + percent);
+    g = g * (1 + percent);
+    b = b * (1 + percent);
+    return "rgb(" + r + "," + g + "," + b + ")";
+}
 window.onload = function () {
     document.getElementById('toggle-menu').onclick = function () {
         var menu = document.getElementById('menu');
@@ -16,5 +44,6 @@ window.onload = function () {
             this.firstChild.innerHTML = UP_ARROW;
         }
     };
+    doPrettyColors();
     minesweeperSetup();
 };
--- scripts/general.ts
+++ scripts/general.ts
@@ -1,6 +1,48 @@
 const UP_ARROW: string = '↑';
 const DOWN_ARROW: string = '↓';
 
+/* Assuming the base color of each .sub-element is already set in hsl, desaturate
+ * that color progressively for each child of that element */
+function doPrettyColors() {
+	let ids = ["first", "second", "third", "fourth", "fifth"];
+
+	for (let id of ids) {
+		let container = document.getElementById(id);
+		let children = container.children;
+		let topColor = window.getComputedStyle((children[0] as HTMLElement)).backgroundColor;
+		// let topLightness = parseInt(topColor.substring(topColor.lastIndexOf(',') + 1));
+		// let i = 0; // use to calculate next lightness value
+		console.log(topColor);
+
+		for (let childIndex in children) { // use index for computed lightness
+			console.log(children);
+			console.log(childIndex);
+			console.log(children[childIndex]);
+			if (parseInt(childIndex)) {
+				let currElement = <HTMLElement> children[childIndex];
+				currElement.style.backgroundColor = decrementLightness(topColor, parseInt(childIndex) * 0.1);
+			}
+		}
+	}
+}
+
+/* Given a string representing an RGB value (like "rgb(255, 255, 255)"), returns
+ * a string representing that color decreased by the percent value, where e.g. a
+ * percent value of 0.1 will decrement by 10% */
+function decrementLightness(colorString: string, percent: number) {
+	// first, get individual values
+	let r: number = parseInt(colorString.substring(4, colorString.indexOf(',')));
+	let g: number = parseInt(colorString.substring(
+						colorString.indexOf(',') + 1, colorString.lastIndexOf(',')));
+	let b: number = parseInt(colorString.substring( colorString.lastIndexOf(',') + 1));
+
+	r = r * (1 + percent);
+	g = g * (1 + percent);
+	b = b * (1 + percent);
+
+	return "rgb(" + r + "," + g + "," + b + ")";
+}
+
 window.onload = function() {
 	document.getElementById('toggle-menu').onclick = function() {
 		let menu = document.getElementById('menu');
@@ -17,6 +59,8 @@ window.onload = function() {
 			(this.firstChild as HTMLElement).innerHTML = UP_ARROW;
 		}
 	};
+
+	doPrettyColors();
 
 	minesweeperSetup();
 }
